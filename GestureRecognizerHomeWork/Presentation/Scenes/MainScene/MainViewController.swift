29d56e4ecf9dde7830bb514769abe961bf156bc6
Giorgi Meskhi <meskhi10@gmail.com>
2021-06-09 15:37:38 +0300

    -add tap gestures for labels

--- GestureRecognizerHomeWork/Presentation/Scenes/MainScene/MainViewController.swift
+++ GestureRecognizerHomeWork/Presentation/Scenes/MainScene/MainViewController.swift
@@ -16,9 +16,61 @@ class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupPanGesture()
+        setupLongPressGesture()
+        setupSwipeGestureLabel()
+        setupPinchGestureLabel()
+        
+    }
+    
+    private func setupPanGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(MainViewController.panGestureTapped))
+        panGestureLabel.isUserInteractionEnabled = true
+        panGestureLabel.addGestureRecognizer(tap)
+    }
+    
+    private func setupLongPressGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(MainViewController.panLongPressGestureTapped))
+        longPressGestureLabel.isUserInteractionEnabled = true
+        longPressGestureLabel.addGestureRecognizer(tap)
+    }
+    
+    private func setupSwipeGestureLabel() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(MainViewController.panSwipeGestureTapped))
+        swipeGestureLabel.isUserInteractionEnabled = true
+        swipeGestureLabel.addGestureRecognizer(tap)
+    }
+    
+    private func setupPinchGestureLabel() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(MainViewController.panPinchGestureTapped))
+        pinchGestureLabel.isUserInteractionEnabled = true
+        pinchGestureLabel.addGestureRecognizer(tap)
     }
 
+    @objc func panGestureTapped(_ sender: UITapGestureRecognizer) {
+        moveToGestoreVC(with: 0)
+    }
+    
+    @objc func panLongPressGestureTapped(_ sender: UITapGestureRecognizer) {
+        moveToGestoreVC(with: 1)
+    }
+    
+    @objc func panSwipeGestureTapped(_ sender: UITapGestureRecognizer) {
+        moveToGestoreVC(with: 2)
+    }
+    
+    @objc func panPinchGestureTapped(_ sender: UITapGestureRecognizer) {
+        moveToGestoreVC(with: 3)
+    }
+    
+    private func moveToGestoreVC(with tag: Int) {
+        let sb = UIStoryboard(name: "GestureViewController", bundle: nil)
+        let gestureVC = sb.instantiateViewController(withIdentifier: "GestureViewController") as! GestureViewController
+        
+        gestureVC.gestureTag = tag
+        
+        self.navigationController?.pushViewController(gestureVC, animated: true)
+    }
 
 }
-
