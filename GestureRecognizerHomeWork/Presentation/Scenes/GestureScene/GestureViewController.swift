d28899c117715cd6cdde5e71c716f8f07123a0e1
Giorgi Meskhi <meskhi10@gmail.com>
2021-06-09 15:09:08 +0300

    -add ui components

---
+++ GestureRecognizerHomeWork/Presentation/Scenes/GestureScene/GestureViewController.swift
@@ -0,0 +1,23 @@
+//
+//  GestureViewController.swift
+//  GestureRecognizerHomeWork
+//
+//  Created by Admin on 09.06.2021.
+//
+
+import UIKit
+
+class GestureViewController: UIViewController {
+
+    @IBOutlet weak var xCoordinatLabel: UILabel!
+    @IBOutlet weak var yCoordinatLabel: UILabel!
+    
+    var gestureTag = Int()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+}
---
+++ GestureRecognizerHomeWork/Presentation/Scenes/MainScene/MainViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GestureRecognizerHomeWork
+//
+//  Created by Admin on 09.06.2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var panGestureLabel: UILabel!
+    @IBOutlet weak var longPressGestureLabel: UILabel!
+    @IBOutlet weak var swipeGestureLabel: UILabel!
+    @IBOutlet weak var pinchGestureLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
